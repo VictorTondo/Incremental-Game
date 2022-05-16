7e58cd04167c20a5a408abb464963037e81a3575
Victor <victortondo@gmail.com>
2022-05-16 14:45:36 +0200

    button added but not working yet

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+let gameData = {
+    gold:0,
+    goldPerClick:1,
+}
+
+function mineGold (){
+
+    gameData.gold += gameData.goldPerClick
+    document.getElementById("goldMined").innerHTML=gameData.gold + "Gold Mined"
+
+}
