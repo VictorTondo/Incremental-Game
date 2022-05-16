69120ec2edcab4df811e9a8a9857359994da5b93
Victor <victortondo@gmail.com>
2022-05-16 15:06:17 +0200

    button and multiplier working

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 let gameData = {
     gold:0,
     goldPerClick:1,
+    goldPerClickCost:10,
 }
 
 function mineGold (){
@@ -11,4 +12,24 @@ function mineGold (){
     gameData.gold += gameData.goldPerClick
     document.getElementById("goldMined").innerHTML=gameData.gold + "Gold Mined"
 
+}
+
+function buyGoldPerClick () {
+    if (gameData.gold >= gameData.goldPerClickCost) {
+
+        gameData.gold -= gameData.goldPerClickCost
+        gameData.goldPerClick += 1
+        gameData.goldPerClickCost *=2
+        document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+        document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
+    }
+
+
+
+
+
+
+    let mainGameLoop = window.setInterval (function (){
+        mineGold()
+    }, 1000)
 }
